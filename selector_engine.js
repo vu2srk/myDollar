9e58a442b56b77595eea0c6f85160a9f137c67cb
Sharadha Ramakrishnan <vu2srk@gmail.com>
2012-11-15 16:38:15 -0500

    added multiple selector functionality and child selection

--- selector_engine.js
+++ selector_engine.js
@@ -1,20 +1,39 @@
 var SelectedNodes = function(selectors, nodes){
+    this.nodes = [];
+    this.selectedNodes = [];
     if (typeof(nodes)==="undefined" || nodes.length==0)
-        nodes = document.getElementsByTagName("*");
-    this.nodes = nodes;
+        var nodes = document.getElementsByTagName("*");
+    for(var i = nodes.length; i--; this.nodes.unshift(nodes[i]));
+    var allNodes = this.nodes;
+    if (typeof(selectors)==="undefined"||selectors.trim()=="")
+        return;
+    var selectedNodes = [];
     regex = /^(?:#([\w\-]+)|(\w+)|\.([\w\-]+))$/;
-    selectorArray = selectors.split(" ");
+    selectorArray = selectors.split(",");
     for (var i=0;i<selectorArray.length;i++){
-        match = regex.exec(selectorArray[i].trim());
-        if (match[0]){
-            if (match[1]){
-                this.nodes = this.hasID(match[1]);
-            } else if (match[2]){
-                this.nodes = this.hasTag(match[2]);
-            } else if (match[3]){
-                this.nodes = this.hasClass(match[3]);
+        var selector = selectorArray[i];
+        var subSelectors = selector.split(/(?=[ #.])/);
+        for (j=0;j<subSelectors.length;j++){
+            match = regex.exec(subSelectors[j].trim());
+            if (match[0]){
+                if (match[1]){
+                    this.nodes = this.hasID(match[1]);
+                } else if (match[2]){
+                    this.nodes = this.hasTag(match[2]);
+                } else if (match[3]){
+                    this.nodes = this.hasClass(match[3]);
+                }
+            }
+            if (j+1 != subSelectors.length){
+                for (var d=0;d<this.nodes.length;d++){
+                    var node = this.nodes[d];
+                    var newNodes = this.nodes[d].getElementsByTagName("*");
+                }
+                for(var x = newNodes.length; x--; this.nodes.unshift(newNodes[x]));
             }
         }
+        this.selectedNodes = this.selectedNodes.concat(this.nodes);
+        this.nodes = allNodes;
     }
 }
 
@@ -66,17 +85,17 @@ SelectedNodes.prototype = {
     },
 
     setBgColor : function(color){
-        for (var i=0;i<this.nodes.length;i++){
-            this.nodes[i].style.backgroundColor = color;
+        for (var i=0;i<this.selectedNodes.length;i++){
+            this.selectedNodes[i].style.backgroundColor = color;
         }
     },
 
     html : function(htmlContent){
         if (typeof(htmlContent)==="undefined" || htmlContent.trim()=="")
-            return this.nodes[0].innerHTML;
+            return this.selectedNodes[0].innerHTML;
         else{
-            for (var i=0;i<this.nodes.length;i++){
-                this.nodes[i].innerHTML = htmlContent;
+            for (var i=0;i<this.selectedNodes.length;i++){
+                this.selectedNodes[i].innerHTML = htmlContent;
             }
         }
     }
