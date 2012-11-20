b4cd0a2ac1a91632a4f2c67f7404c00452be451a
Sharadha Ramakrishnan <vu2srk@gmail.com>
2012-11-20 12:32:21 -0500

    Exposed only user accessible functions. Wrapped my$ in a module (for local scoping)

--- mydollar_test.js
+++ mydollar_test.js
@@ -3,24 +3,24 @@ module("Selector Tests", {});
 test("test class selector", function(){
     expect(1);
     var nodes = my$(".url");
-    equal(nodes.selectedNodes.length, 1);
+    equal(nodes.length, 1);
 });
 
 test("test sub-selector", function(){
     expect(2);
     var nodes = my$(".something p");
-    equal(nodes.selectedNodes.length, 1); 
-    equal(nodes.selectedNodes[0].id, "innerP");
+    equal(nodes.length, 1); 
+    equal(nodes[0].id, "innerP");
 });
 
 test("test multiple selectors", function(){
     expect(1);
     var nodes = my$(".something, .url");
-    equal(nodes.selectedNodes.length, 3);
+    equal(nodes.length, 3);
 });
 
 test("test duplicate selectors", function(){
     expect(1);
     var nodes = my$(".something, #test_world p");
-    equal(nodes.selectedNodes.length, 3);
+    equal(nodes.length, 3);
 });
--- selector_engine.js
+++ selector_engine.js
@@ -1,134 +1,145 @@
+(function(window) {
+	function contains(list, item) {
+		for (var i = 0; i < list.length; i++) {
+			if (item === list[i])
+				return true;
+		}
+		return false;
+	}
 
-function contains(list, item){
-    for (var i=0;i<list.length;i++){
-        if (item === list[i])
-            return true;
-    }
-    return false;
-}
+	function unique(list) {
+		var newList = [];
+		for (var i = 0; i < list.length; i++) {
+			if (!contains(newList, list[i]))
+				newList.push(list[i]);
+		}
+		return newList;
+	}
 
-function unique(list){
-    var newList = [];
-    for (var i=0;i<list.length;i++){
-        if (!contains(newList, list[i]))
-            newList.push(list[i]);
-    }
-    return newList;
-}
+	var my$ = function(selectors) {
+		return new my$.fn.init(selectors);
+	}
 
+	my$.fn = my$.prototype = {
+		init : function(selectors) {
 
-var SelectedNodes = function(selectors){
-    
-    if (typeof selectors === "undefined" || selectors.trim() == "")
-        return;
-     
-    //my$(".something div")
+			if ( typeof selectors === "undefined" || selectors.trim() == "")
+				return;
 
-    var slice = Array.prototype.slice;
+			//my$(".something div")
 
-    this.selectedNodes = [];
-    this.nodes = slice.call(document.getElementsByTagName("*"), 0);
-    var allNodes = this.nodes;
-    var selectedNodes = [];
-    var regex = /^(?:#([\w\-]+)|(\w+)|\.([\w\-]+))$/;
+			var slice = Array.prototype.slice;
+			var push = Array.prototype.push;
 
-    var selectorArray = selectors.split(",");
-    for (var i=0;i<selectorArray.length;i++){
-        var selector = selectorArray[i];
-        var subSelectors = selector.trim().split(/(?=[ #.])/);
-        for (var j=0;j<subSelectors.length;j++){
-            var match = regex.exec(subSelectors[j].trim());
-            if (match[0]){
-                if (match[1]){
-                    this.nodes = this.hasID(match[1]);
-                } else if (match[2]){
-                    this.nodes = this.hasTag(match[2]);
-                } else if (match[3]){
-                    this.nodes = this.hasClass(match[3]);
-                }
-            }
-            if (j+1 != subSelectors.length){
-                var newNodes = [];
-                for (var d=0;d<this.nodes.length;d++){
-                    var node = this.nodes[d];
-                    newNodes = newNodes.concat(slice.call(node.getElementsByTagName("*"),0));
-                }
-                this.nodes = newNodes;
-            }
-        }
-        this.selectedNodes = this.selectedNodes.concat(this.nodes);
-        this.nodes = allNodes;
-    }
-    this.selectedNodes = unique(this.selectedNodes);
-}
+			var selectedNodes = [];
+			var nodes = slice.call(document.getElementsByTagName("*"), 0);
+			var allNodes = nodes;
+			var selectedNodes = [];
+			var regex = /^(?:#([\w\-]+)|(\w+)|\.([\w\-]+))$/;
 
-SelectedNodes.prototype = {
-    throwError : function(msg){
-        throw new Error(msg);
-    },
+			var selectorRegexes = [{
+				re : /^(?:(\w+))$/,
+				fn : hasTag
+			}, {
+				re : /^(?:#([\w\-]+))$/,
+				fn : hasID
+			}, {
+				re : /^(?:\.([\w\-]+))$/,
+				fn : hasClass
+			}];
 
-    hasTag : function(tagName){
-        var newList = [];
-        if (typeof tagName==='undefined')
-            this.throwError("No tag name specified");
-        else {
-            var len = this.nodes.length;
-            for (var i=0;i<len;i++){
-                if (this.nodes[i].tagName == tagName.toUpperCase())
-                    newList.push(this.nodes[i]);
-            }
-        }
-        return newList;
-    },
+			var selectorArray = selectors.split(",");
+			for (var i = 0; i < selectorArray.length; i++) {
+				var selector = selectorArray[i];
+				var subSelectors = selector.trim().split(/(?=[ #.])/);
+				for (var j = 0; j < subSelectors.length; j++) {
+					for (var k = 0; k < selectorRegexes.length; k++) {
+						var match = selectorRegexes[k].re.exec(subSelectors[j].trim());
+						if (match)
+							nodes = selectorRegexes[k].fn(match[1], nodes);
+					}
+					if (j + 1 != subSelectors.length) {
+						var newNodes = [];
+						for (var d = 0; d < nodes.length; d++) {
+							var node = nodes[d];
+							newNodes = newNodes.concat(slice.call(node.getElementsByTagName("*"), 0));
+						}
+						nodes = newNodes;
+					}
+				}
+				selectedNodes = selectedNodes.concat(nodes);
+				nodes = allNodes;
+			}
+			selectedNodes = unique(selectedNodes);
+			push.apply(this, slice.call(selectedNodes, 0));
+			return this;
+		},
+		setBgColor : function(color) {
+			for (var i = 0; i < this.length; i++) {
+				this[i].style.backgroundColor = color;
+			}
+		},
 
-    hasClass : function(className){
-        var newList = [];
-        if(typeof className === 'undefined')
-            this.throwError("No class name specified");
-        else{
-            var len = this.nodes.length;
-            for (var i=0;i<len;i++){
-                if (this.nodes[i].className == className)
-                    newList.push(this.nodes[i]);
-            }
-        }
-        return newList;
-    },
+		html : function(htmlContent) {
+			if ( typeof htmlContent === "undefined" || htmlContent.trim() == "")
+				return this[0].innerHTML;
+			else {
+				for (var i = 0; i < this.length; i++) {
+					this[i].innerHTML = htmlContent;
+				}
+			}
+		}
+	}
 
-    hasID : function(id){
-        var newList = [];
-        if(typeof id ==='undefined')
-            this.throwError("No id specified");
-        else{
-            var len = this.nodes.length;
-            for (var i=0;i<len;i++){
-                if (this.nodes[i].id == id)
-                    newList.push(this.nodes[i]);
-            }
-        }
-        return newList;
-    },
+	my$.fn.init.prototype = my$.prototype;
 
-    setBgColor : function(color){
-        for (var i=0;i<this.selectedNodes.length;i++){
-            this.selectedNodes[i].style.backgroundColor = color;
-        }
-    },
+	function throwError(msg) {
+		throw new Error(msg);
+	}
 
-    html : function(htmlContent){
-        if (typeof htmlContent === "undefined" || htmlContent.trim()=="")
-            return this.selectedNodes[0].innerHTML;
-        else{
-            for (var i=0;i<this.selectedNodes.length;i++){
-                this.selectedNodes[i].innerHTML = htmlContent;
-            }
-        }
-    }
-    
-};
+	function hasTag(tagName, nodes) {
+		var newList = [];
+		if ( typeof tagName === 'undefined')
+			throwError("No tag name specified");
+		else {
+			var len = nodes.length;
+			for (var i = 0; i < len; i++) {
+				if (nodes[i].tagName == tagName.toUpperCase())
+					newList.push(nodes[i]);
+			}
+		}
+		return newList;
+	}
 
-var my$ = function(selectors){
-    return new SelectedNodes(selectors);
-}
+	function hasClass(className, nodes) {
+		var newList = [];
+		if ( typeof className === 'undefined')
+			throwError("No class name specified");
+		else {
+			var len = nodes.length;
+			for (var i = 0; i < len; i++) {
+				if (nodes[i].className == className)
+					newList.push(nodes[i]);
+			}
+		}
+		return newList;
+	}
 
+	function hasID(id, nodes) {
+		var newList = [];
+		if ( typeof id === 'undefined')
+			throwError("No id specified");
+		else {
+			var len = nodes.length;
+			for (var i = 0; i < len; i++) {
+				if (nodes[i].id == id)
+					newList.push(nodes[i]);
+			}
+		}
+		return newList;
+	}
+
+	window.my$ = my$;
+
+})(window)
 
